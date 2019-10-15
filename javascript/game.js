a9c3e5838388c79e619404b24b78ef023a105e31
tinashe <giyavha_tinashe@berkeley.edu>
2019-10-14 18:34:46 -0700

    Adds js and style

--- javascript/game.js
+++ javascript/game.js
@@ -1,35 +1,157 @@
-let score = 0;
-let losses = 0;
-let guessesRemainig = 12;
+//VARIABLES
+var words = ["sheep", "swan"]
 
-let animals = ['lion', 'zebra', 'owl']
+//Empty variables to store values later
+var randomWord = "";
+var lettersOfWord = []
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongGuess = [];
+
+//Counter Variables
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 9;
 
 
 
-function updateScore() {
-   var update = document.getElementById("wins");
-    update.textContent = "Score: " + score;
+
+//__________________________________________________________
+//GAME START FUNCTION
+//__________________________________________________________
+function Game() {
+    //computer generates random word from words array
+    randomWord = words[Math.floor(Math.random() * words.length)];
+
+    // split the individual word into separate arrays, and store in new array 
+    lettersOfWord = randomWord.split("");
+
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length;
+
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    //showing the "_" within HTML
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+
+    //console logging 
+    console.log(randomWord);
+    console.log(lettersOfWord)
+    console.log(blanks)
+    console.log(blanksAndCorrect)
+}
+//__________________________________________________________
+//AUDIO FUNCTION
+//__________________________________________________________
+
+//variables for audio function
+var sheep = document.getElementById("sheep");
+var swan = document.getElementById("swan");
+
+
+function aud() {
+   
+    //---------------------------
+    if (randomWord === words[0]) {
+        swan.pause();
+        sheep.play();
+        document.getElementById("image").href = "https://www.google.com/logos/fnbx/animal_sounds/sheep.png";
+    }
+    //Swan Audio & Image
+    //---------------------------
+    else if (randomWord === words[1]) {
+        sheep.pause();
+        swan.play();
+        document.getElementById("image").href = "https://www.google.com/logos/fnbx/animal_sounds/swan.png";
+    }
+
+    
 };
 
-let current = animals[0]
+//__________________________________________________________
+//RESET FUNCTION
+//__________________________________________________________
+function reset() {
+    guessesRemaining = 9;
+    wrongGuess = [];
+    blanksAndCorrect = [];
+    Game()
+}
 
-let guessed = 'o'
+//__________________________________________________________
+//CHECK LETTERS/COMPARE FUNCTION
+//__________________________________________________________
 
-
-function updateAnimal() {
-    var update = document.getElementById("wins");
-    placeHolder = "_".repeat(current.length)
-    update.textContent = "Animal: " + placeHolder;
-    if (current.indexOf(guessed)!==-1) {
-        temp = placeHolder.split("");
-        temp[current.indexOf(guessed)] = guessed;
-        placeHolder = temp.join("");
+//If/Else, to see if letter selected matches random word
+function checkLetters(letter) {
+    var letterInWord = false;
+    //if the generated randomword is equal to the letter entered... then variable is true
+    for (var i = 0; i < blanks; i++) {
+        if (randomWord[i] == letter) {
+            letterInWord = true;
+        }
     }
-    update.textContent = "Animal: " + placeHolder;
- };
+    //if letterInWord (false)
+    if (letterInWord) {
+        //check each letter to see if it matches word
+        for (var i = 0; i < blanks; i++) {
+            if (randomWord[i] == letter) {
+                blanksAndCorrect[i] = letter;
+            }
+        }
+    }
+    //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
+    else {
+        wrongGuess.push(letter);
+        guessesRemaining--;
+    }
+    console.log(blanksAndCorrect);
+}
 
+//__________________________________________________________
+//FINAL COMPLETE FUNCTION
+//__________________________________________________________
 
- 
+//check to see if player won...
+function complete() {
+    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
 
-updateScore();
-updateAnimal();
+    //if WON...then alert, play audio, display image and reset new round
+    if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
+        wins++;
+        aud()
+        reset()
+        //display wins on screen
+        document.getElementById("winstracker").innerHTML = " " + wins;
+
+        //if LOST...then alert and reset new round
+    } else if (guessesRemaining === 0) {
+        losses++;
+        reset()
+        document.getElementById("image").src = "images/try-again.png"
+        document.getElementById("losstracker").innerHTML = " " + losses;
+    }
+    //display losses on screen && guesses remaining countdown
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join(" ");
+    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
+}
+
+//call start game function
+Game()
+
+//check for keyup, and convert to lowercase then store in guesses
+document.onkeyup = function (event) {
+    var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+    //check to see if guess entered matches value of random word
+    checkLetters(guesses);
+    //process wins/loss 
+    complete();
+    //store player guess in console for reference 
+    console.log(guesses);
+
+    //display/store incorrect letters on screen
+    document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
+}
